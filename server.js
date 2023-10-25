64b2bfcacf161c0eb0f53c1498d8c38388df478a
dhruvermafz <vermadhruv09112002@gmail.com>
2023-10-25 09:22:43 +0530

    local host updated

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const errorHandlerMiddleware = require("./middlewares/errorHandler");
 
 app.use(express.json());
 const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
+  origin: ["http://localhost:3000", "https://live-auctions.vercel.app"],
+  credentials: true,
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
