d8cd34a85866ff4396153e480c38ee0b2c888e65
dhruvermafz <vermadhruv09112002@gmail.com>
2023-12-28 12:30:41 +0530

    changes upload

--- routes/uploads.js
+++ routes/uploads.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const isAuth = require("../middlewares/isAuth");
 const uploadController = require("../controllers/upload");
 
-router.post("/upload", isAuth, uploadController.uploadImage);
+router.post("/uploadImage", isAuth, uploadController.uploadImage);
 
 module.exports = router;
