6eda822cf968c6f5e50cbc5a245e39b611c1d241
dhruvermafz <vermadhruv09112002@gmail.com>
2023-12-31 19:02:59 +0530

    added multiple controllers

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,7 +1,14 @@
 const cloudinary = require("cloudinary").v2;
-const formidable = require("formidable");
+const multer = require("multer");
 const { v4: uuidv4 } = require("uuid");
 require("dotenv").config();
+
+const storage = multer.memoryStorage(); // Store files in memory
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 }, // Set file size limit to 5 MB
+});
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -9,25 +16,23 @@ cloudinary.config({
   api_secret: process.env.SECRET,
 });
 
-exports.uploadImage = async (req, res) => {
-  try {
-    // Basic user authentication check (customize as needed)
-    if (!req.userid) {
-      return res.status(401).json({ error: "Unauthorized" });
-    }
+exports.uploadImage = [
+  upload.single("image"), // "image" should match the name attribute in your HTML form
+  async (req, res) => {
+    try {
+      // Basic user authentication check (customize as needed)
+      if (!req.userid) {
+        return res.status(401).json({ error: "Unauthorized" });
+      }
 
-    const form = formidable({ multiples: true });
-
-    form.parse(req, async (err, fields, files) => {
-      if (err) {
-        console.error("Formidable error:", err);
-        return res.status(500).json({ error: "Internal Server Error" });
+      if (!req.file) {
+        return res.status(400).json({ error: "No file provided" });
       }
 
       // Generate a unique identifier for the image using uuid
       const uniqueIdentifier = uuidv4();
 
-      const image = await cloudinary.uploader.upload(files.image.path, {
+      const image = await cloudinary.uploader.upload(req.file.buffer, {
         resource_type: "image",
         public_id: `users/${req.userid}/${uniqueIdentifier}`,
         crop: "scale",
@@ -37,9 +42,9 @@ exports.uploadImage = async (req, res) => {
       console.log("Image uploaded:", image.secure_url);
 
       return res.status(200).json({ image: image.secure_url });
-    });
-  } catch (error) {
-    console.error("Upload error:", error);
-    return res.status(500).json({ error: "Internal Server Error" });
-  }
-};
+    } catch (error) {
+      console.error("Upload error:", error);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  },
+];
